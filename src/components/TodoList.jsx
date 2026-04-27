1510a5b8d33a22cdda2838bce84a4a8065975f95
saharshirzad2026-creator <saharshirzad2026@gmail.com>
2026-04-26 22:23:13 -0700

    todolist component

---
+++ src/components/TodoList.jsx
@@ -0,0 +1,11 @@
+import TodoCard from "./TodoCard";
+
+export default function TodoList({list, setList}){
+    return (
+        <div className="w-full max-w-6xl flex flex-col gap-2.5 my-28 border border-violet-600 shadow-2xl shadow-black rounded-md mx-auto mb-2">
+            {list.map(l=>(
+                <TodoCard todo = {l} setList = {setList}/>
+            ))}
+        </div>
+    )
+}
