61e9982e6d9689433b95a22a0409d147b60360b4
saharshirzad2026-creator <saharshirzad2026@gmail.com>
2026-05-24 19:33:28 -0700

    first reducer task

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,24 @@ import CreateForm from "./components/form";
 import Navbar from "./components/nav";
 import TodoList from "./components/TodoList";
 import FirstReducer from "./components/FirstReducer";
+import SecondReducer from "./components/SecondReducer";
+import ThirdReducer from "./components/ThirdReducer";
+import FourthReducer from "./components/FourthReducer";
+import FifthReducer from "./components/FifthReducer";
 
 export default function App(){
   const [showForm, setShowForm] = useState(false);
   const [list, setList] = useState([]);
   return (
-    <>
+    <div className="bg-stone-200 w-full min-h-screen">
       {/* <Navbar showForm = {showForm} setShowForm = {setShowForm}/>
             <CreateForm list = {list} setList = {setList} showForm = {showForm} setShowForm = {setShowForm}/>
             <TodoList list = {list} setList = {setList}/> */}
-            <FirstReducer/>
-    </>
+            {/* <FirstReducer/> */}
+            {/* <SecondReducer/> */}
+            <ThirdReducer/>
+            <FourthReducer/>
+            {/* <FifthReducer/> */}
+    </div>
   )
 }
--- src/components/FirstReducer.jsx
+++ src/components/FirstReducer.jsx
@@ -12,10 +12,13 @@ function FirstReducer() {
     }
     const [data, action] = useReducer(func, {count: 0})
   return (
-    <div className='w-1/2 h-72 border bg-white flex flex-col gap-10 mx-auto mt-40 rounded-md'>
-      <h1 className='text-2xl font-bold'>{data.count}</h1>
-      <button className='py-3 px-20 mx-6 rounded-md bg-linear-60 from-emerald-700 via-emerald-300 to-emerald-600 text-white text-xl font-bold font-serif' onClick={()=>action({type: "inc", payload: 1})}>Increment</button>
-      <button className='py-3 px-20 mx-6 rounded-md bg-linear-60 from-emerald-700 via-emerald-300 to-emerald-600 text-white text-xl font-bold font-serif' onClick={()=>action({type: "dec", payload: 1})}>Decrement</button>
+    <div className='bg-stone-200 w-full h-screen'>
+    <div className='w-1/2 h-96 shadow-2xl shadow-gray-400 bg-white flex flex-col gap-10 mx-auto rounded-md'>
+    <h1 className='text-3xl font-bold text-center bg-linear-120 from-emerald-600 via-emerald-400 to-emerald-700 text-transparent bg-clip-text font-serif'>First Task of useReducer</h1>
+      <button className='py-3 px-20 mx-25 hover:cursor-pointer rounded-md bg-linear-60 from-emerald-700 via-emerald-400 to-emerald-600 text-white text-xl font-bold font-serif' onClick={()=>action({type: "inc", payload: 1})}>Increment</button>
+      <h1 className='text-2xl font-bold rounded-xl text-white px-4 w-40 mx-auto py-3 text-center bg-emerald-600'>{data.count}</h1>
+      <button className='py-3 px-5 mx-25 hover:cursor-pointer rounded-md bg-linear-60 from-emerald-700 via-emerald-400 to-emerald-600 text-white text-xl font-bold font-serif' onClick={()=>action({type: "dec", payload: 1})}>Decrement</button>
+    </div>
     </div>
   )
 }
