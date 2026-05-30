f70d5d3c07b7d6bd972579628675db4512360046
saharshirzad2026-creator <saharshirzad2026@gmail.com>
2026-05-29 20:54:26 -0700

    a part of todo

---
+++ src/components/SecondTodo.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function SecondTodo({t}) {
+  return (
+    <div className='p-6 rounded-md bg-white shadow-2xl shadow-stone-300'>
+      <h1 className='text-3xl font-bold bg-linear-60 from-indigo-600 via-indigo-400 to-indigo-600 text-transparent bg-clip-text'>
+        {t}
+      </h1>
+    </div>
+  )
+}
+
+export default SecondTodo
