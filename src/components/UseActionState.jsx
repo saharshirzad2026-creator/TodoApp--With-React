6ae0798045dded3508abec88edc85eb1214a3ca7
saharshirzad2026-creator <saharshirzad2026@gmail.com>
2026-06-24 01:03:24 -0700

    useActionState component

--- src/components/UseActionState.jsx
+++ src/components/UseActionState.jsx
@@ -1,8 +1,10 @@
 import React, { useActionState } from 'react'
 import { getDataFromForm } from '../Actions/User.action'
+import { useFormStatus } from 'react-dom'
 
 function UseActionState() {
-    const [data, func] = useActionState(getDataFromForm,{
+    const status = useFormStatus();
+    const [data, func, loading] = useActionState(getDataFromForm,{
         message: '',
         success: false,
     })
@@ -10,11 +12,15 @@ function UseActionState() {
     <div className='w-full flex flex-col'>
     <div className='w-full min-h-screen justify-center items-center flex'>
       <form 
-      className='w-7/12 border p-8 rounded-md flex flex-col gap-4' 
+      className='w-7/12 border p-8 rounded-md flex flex-col gap-4'
       action={func}>
         <input name='name' className='border px-2 py-2 w-full rounded-md focus:outline-0' type="text" placeholder='Enter your Name'/>
         <input name='email' className='border px-2 py-2 w-full rounded-md focus:outline-0' type="email" placeholder='Enter your Email address'/>
-        <button className='py-2 hover:cursor-pointer w-full text-xl font-bold rounded-md bg-linear-60 from-fuchsia-500 via-fuchsia-300 to-fuchsia-800 text-stone-100' type='submit'>Save</button>
+        <button
+        disabled={status.pending} 
+        className='py-2 hover:cursor-pointer w-full text-xl font-bold rounded-md bg-linear-60 from-fuchsia-500 via-fuchsia-300 to-fuchsia-800 text-stone-100' type='submit'>
+            {loading ? 'Please wait...' : 'Submit'}
+        </button>
       </form>
     </div>
     {data.success && (
